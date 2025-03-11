ec3a46cc9c115f3c96a3845fe78dc986c6711042
intruq <paul.dirksenweb.de@gmail.com>
2025-03-11 16:55:08 +0100

    Change file names, added directories for attack scenario files

--- demkit/components/util/manualMDI.py
+++ demkit/components/util/manualMDI.py
@@ -1,4 +1,7 @@
 import csv
+import shutil
+
+path = "C:/Users/intruq/Desktop/Bachelorarbeit/DEMKit/workspace/example/data/"
 
 def makeCSVs():
     joinCsv("CSV-House-1", ["SmartMeter-House-1", "Load-House-1", "WashingMachine-House-1", "Dishwasher-House-1", "ElectricVehicle-House-1", "Heatpump-House-1", "DomesticHotWaterControllerBoiler-House-1", "PV-House-1"])
@@ -8,85 +11,93 @@ def makeCSVs():
     shortenCSV("CSV-House-1")
     shortenCSV("CSV-House-4")
     shortenCSV("CSV-House-8")
+    feeder("")
 
-    #modifierS1(True)
-    #modifierS1(False)
-    #modifierS2()
-    #modifierS3(True)
-    #modifierS3(False)
-    #modifierS4()
-    #modifierS5()
-    #modifierS6()
+    att_scenario_1()
+    att_scenario_2()
+    att_scenario_3()
+    #att_scenario_4()
 
-def modifierS1(option: bool):
-    data = readCsv("CSV-House-8-short")
+def att_scenario_1():
+    shutil.copy(path + "CSV-House-1-short.csv", path + "att_scenario_1/CSV-House-1.csv")
+    shutil.copy(path + "CSV-House-4-short.csv", path + "att_scenario_1/CSV-House-4.csv")
+    shutil.copy(path + "CSV-House-8-short.csv", path + "att_scenario_1/CSV-House-8.csv")
+    feeder("att_scenario_1/")
 
-    if(option): # Smart Meter Value incorrect
-        for i in range(1, len(data)):
-            data[i][1] = float(data[i][1]) + 100
-        writeCsv("CSV-MOD-S1-1", data) 
-    else: # Device Values incorrect
-        for i in range(1, len(data)):
-            data[i][4] = float(data[i][4]) + 100
-            data[i][7] = float(data[i][7]) + 100
-            data[i][8] = float(data[i][8]) + 100
-        writeCsv("CSV-MOD-S1-2", data)
+def att_scenario_2():
+    as1_var_1()
+    as1_var_2()
+    as1_var_3()
 
-def modifierS2():
-    data = readCsv("CSV-Smart-Meters-short")
-    for i in range(1, len(data)):
-            data[i][i % 10] = 12001
-    writeCsv("CSV-MOD-S2", data)
+def att_scenario_3():
+    data1, data2 = readCsv("CSV-House-1-short"), readCsv("CSV-House-8-short")
+    for i in range(1, len(data1)):
+        if(i % 2 == 1):
+            data1[i][1] = float(data1[i][1]) - float(data1[i][4])
+            data2[i][1] = float(data2[i][1]) - float(data2[i][4])
+            data1[i][4] = 0
+            data2[i][4] = 0
+        else:
+            data1[i][4] = -11999
+            data2[i][4] = -11999
+            data1[i][1] = float(data1[i][1]) - 11999
+            data2[i][1] = float(data2[i][1]) - 11999
+    writeCsv("att_scenario_3/CSV-House-1", data1)
+    writeCsv("att_scenario_3/CSV-House-8", data2)
+    shutil.copy(path + "CSV-House-4-short.csv", path + "att_scenario_3/CSV-House-4.csv")
+    feeder("att_scenario_3/")
 
-def modifierS3(option: bool):
-    data = readCsv("CSV-House-8-short")
+def att_scenario_4():
+    feeder("att_scenario_4/")
 
-    if(option): # Battery soc over/under 100%/0% (12000 Wh Capacity)
-        for i in range(1, len(data)):
-            if(i % 2 == 1):
-                data[i][3] = 12001
-            else:
-                data[i][3] = -1
-        writeCsv("CSV-MOD-S3-1", data) 
-    else: # Battery Charging while full/discharging while empty
-        for i in range(1, len(data)):
-            if(i % 2 == 1):
-                data[i][3] = 12000 #soc
-                data[i][2] = 100 #current
-            else:
-                data[i][3] = 0 #soc
-                data[i][2] = -100 #current
-        writeCsv("CSV-MOD-S3-2", data) 
-
-def modifierS4():
+def as1_var_1():
     data = readCsv("CSV-House-8-short")
     for i in range(1, len(data)):
-            data[i][4] = -1
-    writeCsv("CSV-MOD-S4", data)
+        data[i][5] = 13000 # Max capacity of power line: 12000W
+        data[i][6] = 4000 # Max capacity of Medium ALPG battery: 4000Wh 
+    writeCsv("att_scenario_2/var1/CSV-House-8", data)
+    shutil.copy(path + "CSV-House-1-short.csv", path + "att_scenario_2/var1/CSV-House-1.csv")
+    shutil.copy(path + "CSV-House-4-short.csv", path + "att_scenario_2/var1/CSV-House-4.csv")
+    feeder("att_scenario_2/var1/")
 
-def modifierS5():
+def as1_var_2():
     data = readCsv("CSV-House-8-short")
     for i in range(1, len(data)):
-            if(i % 2 == 1):
-                data[i][2] = 3800
-            else:
-                data[i][2] = -3800
-    writeCsv("CSV-MOD-S5", data)
+        if(i % 2 == 1):
+            data[i][6] = 5000 # Max capacity of Medium ALPG battery: 4000Wh 
+        else:
+            data[i][6] = -1
+    writeCsv("att_scenario_2/var2/CSV-House-8", data)
+    shutil.copy(path + "CSV-House-1-short.csv", path + "att_scenario_2/var2/CSV-House-1.csv")
+    shutil.copy(path + "CSV-House-4-short.csv", path + "att_scenario_2/var2/CSV-House-4.csv")
+    feeder("att_scenario_2/var2/")
 
-def modifierS6():
-    data = readCsv("CSV-Smart-Meters-short")
+def as1_var_3():
+    data = readCsv("CSV-House-8-short")
     for i in range(1, len(data)):
-            if i % 3 == 0:
-                var = 0
-            elif i % 3 == 1:
-                var = 5000
-            else:
-                var = 10000
-            data[i][1] = var
-    writeCsv("CSV-MOD-S6", data)
+        data[i][5] = (-1) * float(data[i][5])
+    writeCsv("att_scenario_2/var3/CSV-House-8", data)
+    shutil.copy(path + "CSV-House-1-short.csv", path + "att_scenario_2/var3/CSV-House-1.csv")
+    shutil.copy(path + "CSV-House-4-short.csv", path + "att_scenario_2/var3/CSV-House-4.csv")
+    feeder("att_scenario_2/var3/")
 
-def modifierS7():
-    True
+# Function collecting SmartMeter data and adding values to get combined input/output of all houses
+def feeder(folder: str):
+    data, sourceData = [["time", "W-power.real.c.ELECTRICITY"]], []
+    for m in ["CSV-House-1", "CSV-House-4", "CSV-House-8"]:
+        sourceData.append(readCsv(folder + m))
+    try:
+        maxTime = int(sourceData[0][-1][0])
+    except IndexError:
+        maxTime = 0
+    if maxTime > 0:
+        for i in range(1, (maxTime // 60) - 1):
+            time = sourceData[0][i][0]
+            sum_meters = 0
+            for j in range(3):
+                sum_meters += float(sourceData[j][i][1])
+            data.append([time, sum_meters])
+        writeCsv(folder + "CSV-Feeder", data)
 
 # Removes all columns not needed in the IDS for clarity
 def shortenCSV(source: str):
@@ -162,7 +173,7 @@ def readCsv(fileName):
         fileName += '.csv'
 
     # Reading data from a CSV file
-    with open("C:/Users/intruq/Desktop/Bachelorarbeit/DEMKit/workspace/example/data/" + fileName, mode='r', newline='') as file:
+    with open(path + fileName, mode='r', newline='') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
             data.append(row)
@@ -175,7 +186,7 @@ def writeCsv(fileName, data: list):
         fileName += '.csv'
     
     # Writing data to a CSV file
-    with open("C:/Users/intruq/Desktop/Bachelorarbeit/DEMKit/workspace/example/data/" + fileName, mode='w', newline='') as file:
+    with open(path + fileName, mode='w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerows(data)
 
